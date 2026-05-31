bfb24c00563886e2227db01c04fcbaf6c7e28b0e
anton0801 <danilovanton724@gmail.com>
2026-05-31 14:43:22 +0200

    Initial Commit

---
+++ NestRoute/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NestRoute
+//
+//  Created by Антон on 31/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NestRoute/NestRouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  NestRouteApp.swift
+//  NestRoute
+//
+//  Created by Антон on 31/5/26.
+//
+
+import SwiftUI
+
+@main
+struct NestRouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
